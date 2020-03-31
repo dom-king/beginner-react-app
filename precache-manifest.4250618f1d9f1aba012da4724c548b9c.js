b0982e90ff90bef906d9a83b6074493169182df2
Dom King <dminicking91@gmail.com>
2020-03-31 17:19:16 +0100

    Updates

---
+++ precache-manifest.4250618f1d9f1aba012da4724c548b9c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9d17b60eac2a5b5a491bb4cd269a81ec",
+    "url": "/beginner-react-app/index.html"
+  },
+  {
+    "revision": "7c5ca67fdb33a5163162",
+    "url": "/beginner-react-app/static/css/main.91fa4388.chunk.css"
+  },
+  {
+    "revision": "a2e4be9481a54b890f97",
+    "url": "/beginner-react-app/static/js/2.7aaef059.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/beginner-react-app/static/js/2.7aaef059.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7c5ca67fdb33a5163162",
+    "url": "/beginner-react-app/static/js/main.11b5b5da.chunk.js"
+  },
+  {
+    "revision": "808783a508e8aa5a2c28",
+    "url": "/beginner-react-app/static/js/runtime-main.1f4dd263.js"
+  }
+]);
